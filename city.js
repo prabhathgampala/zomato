408559a1622bc6a8751ed8124452c7c2e0ba391f
prabhathgampala <145183780+prabhathgampala@users.noreply.github.com>
2024-09-02 08:01:03 +0530

    Add files via upload

---
+++ city.js
@@ -0,0 +1,40 @@
+export const city= [
+    {
+        name : "Jubilee Hills",
+        place: 388,
+    },
+    {
+        name : "Gachibowli",
+        place: 757,
+    },
+    {
+        name : "Banjara Hills",
+        place: 398,
+    },
+    {
+        name : "Hitech City ",
+        place: 349,
+    },
+    {
+        name : "Madhapur",
+        place: 825,
+    },
+    {
+        name : "Kondapur",
+        place: 562,
+    },
+    {
+        name : "Kukatpally",
+        place: 1389,
+    },
+    {
+        name : "Begumpet",
+        place: 155,
+    },
+    {
+        name : "see more",
+        place: "",
+        class: "seemore"
+    },
+
+]
